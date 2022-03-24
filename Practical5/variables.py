4b2765ef119b6b53105b56c8f175d584cd951a8b
Yunhang2004 <yunhang.21@intl.zju.edu.cn>
2022-03-24 20:06:39 +0800

    added variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,21 @@
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+if d<e:
+	print('e is greater')
+#The rate of new cases is greater in 2021.
+
+x=1<2
+y=3<4
+print(x,y,x and y,x or y)
+m=1>2
+n=3>4
+print(m,n,m and n,m or n)
+p=1<2
+q=3>4
+print(p,q,p and q,p or q)
+w=1>2
+z=3<4
+print(w,z,w and z,w or z)
