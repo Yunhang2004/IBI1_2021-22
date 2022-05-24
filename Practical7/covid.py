6a3bbd2a6433b12e5b9d3c06e539650343b09003
Yunhang2004 <yunhang.21@intl.zju.edu.cn>
2022-05-24 21:34:46 +0800

    changed covid.py

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -5,17 +5,21 @@ print(covid_data.iloc[10:20,[0,2]])
 location=[False,True,False,False,True,False]
 print(covid_data.loc[(covid_data['location']=='Afghanistan'),location])
 
-a=[True,False,True,True,False,False]
-b=[False,False,True,False,False,False]
-c=[False,False,False,True,False,False]
+a=[True,False,True,True,False,False] # date new_cases new_deaths
+b=[False,False,True,False,False,False] # new_case
+c=[False,False,False,True,False,False] # new_deaths
 china_new_data=covid_data.loc[(covid_data['location']=='China'),a]
 china_new_cases=covid_data.loc[(covid_data['location']=='China'),b]
 china_new_deaths=covid_data.loc[(covid_data['location']=='China'),c]
+
 import numpy as np
-print(np.mean(china_new_cases))
-print(np.mean(china_new_deaths))
+
+print(np.mean(china_new_cases,axis=0))
+print(np.mean(china_new_deaths,axis=0))
+
 
 import matplotlib.pyplot as plt
+
 plt.boxplot(china_new_cases,
 	    vert=False,
 	    meanline=True,
@@ -23,6 +27,8 @@ plt.boxplot(china_new_cases,
 		)
 plt.title('New Cases in China')
 plt.show()
+
+
 plt.boxplot(china_new_deaths,
 	    vert=False,
 	    meanline=True,
@@ -36,6 +42,7 @@ plt.xlabel('Date')
 plt.ylabel('New Cases')
 plt.title('New Cases in China over Time')
 plt.show()
+
 #Hypothesis:b(blue),r(red) are colors, while + and o indicate the shape of the dots.(I found this funny hahaha)
 plt.plot(china_new_data['date'],china_new_data['new_deaths'],'ro')
 plt.xlabel('Date')
@@ -44,16 +51,17 @@ plt.title('New Deaths in China over Time')
 plt.show()
 
 #question.txt
-p=(True,False,True,False,True,False)
+p=[True,False,True,False,True,False] # date new_cases total_cases
 spain=covid_data.loc[(covid_data['location']=='Spain'),p]
+
 plt.plot(spain['date'],spain['new_cases'],'bo')
 plt.xlabel('Date')
 plt.ylabel('New cases')
 plt.title('New Cases in Spain over Time')
 plt.show()
 
-plt.plot(spain['date'],spain['total_cases'],'po')
+plt.plot(spain['date'],spain['total_cases'],'bo')
 plt.xlabel('Date')
-plt.ylabel('New Cases')
+plt.ylabel('Total Cases')
 plt.title('Total Cases in Spain over Time')
 plt.show()
